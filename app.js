705c260f9d4a0386ec6ddfd91091d8aad8bcedb9
mc222sn <mc222sn@student.lnu.se>
2018-10-21 16:00:26 +0200

    Adding shuffle method to Deck module

--- app.js
+++ app.js
@@ -3,3 +3,9 @@
 const Deck = require('./src/deck')
 
 let deck = new Deck()
+
+console.log(deck.cards)
+
+deck.shuffle()
+
+console.log(deck.cards)
--- src/deck.js
+++ src/deck.js
@@ -6,17 +6,27 @@
  * @version 1.0
  */
 
-
 class Deck {
-  constructor() {
+  constructor () {
     this.cards = []
     this.values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
     this.suits = ['♦', '♠', '♣', '♥']
 
     for (let suit in this.suits) {
-        for (let value in this.values) {
-          this.cards.push(`${this.values[value]} ${this.suits[suit]}`)
-        }
+      for (let value in this.values) {
+        this.cards.push(`${this.values[value]} ${this.suits[suit]}`)
+      }
+    }
+  }
+
+  shuffle () {
+    let b = 0;
+
+    for (let a = this.cards.length - 1; a > 0; a -= 1) {
+      b = Math.floor(Math.random() * (a + 1))
+      let temp = this.cards[a]
+      this.cards[a] = this.cards[b]
+      this.cards[b] = temp
     }
   }
 }
