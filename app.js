16982cca291b5f980edcd56a212c6936801bbf31
mc222sn <mc222sn@student.lnu.se>
2018-10-23 01:18:34 +0200

    Chaning code inside while loop

--- app.js
+++ app.js
@@ -4,25 +4,26 @@ const Game = require('./src/game')
 const Deck = require('./src/deck')
 const Player = require('./src/player')
 
-let game = new Game('21', 3)
+let game = new Game('21', 10)
 let deck = new Deck()
 let dealer = new Player('Dealer', 18)
 
 let players = []
 for (let i = 0; i < game.players; i++) {
-  let randomNumber = Math.floor(Math.random() * 11) + 8
+  //let randomNumber = Math.floor(Math.random() * 11) + 8
+  let randomNumber = 18
   players[i] = new Player('Player #' + (i + 1), randomNumber)
 }
 
 while (game.isRunning) {
-  console.log('Running game.')
+  //console.log('Running game.')
   // Starting with a shuffle of the deck to have a random order
   deck.shuffle(deck.cards)
-  console.log('Shuffling cards.')
+  //console.log('Shuffling cards.')
 
   // Going through all the players
   for (let a = 0; a < players.length; a++) {
-    let satisfied
+    let satisfied = undefined
     // Going through all the possible cards the player can take (5)
     for (let b = 0; b < 4; b++) {
       if (deck.cards < 2) {
@@ -34,33 +35,32 @@ while (game.isRunning) {
         players[a].insertCard(deck.getCard(deck.cards))
       }
 
-      if (players[a].totalValueOfCards <= 21) {
-        console.log('Points is lower than 21 or equal.')
-        // Checking the value and how many cards the player have and do the following
-        if (players[a].cards.length === 2) {
-          let points = players[a].totalValueOfCards
+      // Defining variables for readability
+      let playerTotalValueOfCards = players[a].totalValueOfCards
+      let playerAmountOfCards = players[a].cards.length
+      let playerCards = players[a].cards
+      let playerStopValue = players[a].stopValue
+      let playerName = players[a].name
 
-          if (points >= players[a].stopValue) {
-            satisfied = true
-            console.log('Satisfied!')
-          }
-        } else if (players[a].cards.length > 2) {
-          let points = players[a].totalValueOfCards
+      if (playerTotalValueOfCards <= 21) {
+        if (playerAmountOfCards === 2) {
 
-          if (points >= players[a].stopValue) {
+          if (playerTotalValueOfCards >= playerStopValue) {
             satisfied = true
-            console.log('Satisfied!')
           }
-        } else if (players[a].totalValueOfCards === 21) {
-          console.log('Player wins!')
-          game.playerWins(players[a].name, players[a].cards, dealer.name)
-          deck.throwedCards.push(players[a].cards)
+        } else if (playerAmountOfCards > 2) {
+
+          if (playerTotalValueOfCards >= playerStopValue) {
+            satisfied = true
+          }
+        } else if (playerTotalValueOfCards === 21) {
+          game.playerWins(playerName, playerCards, playerTotalValueOfCards, dealer.name)
+          deck.throwedCards.push(playerCards)
           break
         }
       } else {
-        console.log('Points are greater than 21')
-        game.dealerWins(players[a].name, players[a].cards, dealer.name, dealer.cards)
-        deck.throwedCards.push(players[a].cards)
+        game.dealerWins(playerName, playerCards, playerTotalValueOfCards, dealer.name, dealer.cards, dealer.totalValueOfCards)
+        deck.throwedCards.push(playerCards)
         deck.throwedCards.push(dealer.cards)
         break
       }
--- src/game.js
+++ src/game.js
@@ -7,19 +7,19 @@ class Game {
     this.isRunning = true
   }
 
-  playerWins (playerName, playerCards, dealerName) {
+  playerWins (playerName, playerCards, playerValueOfCards, dealerName) {
     console.log(
-      `${playerName}: ${playerCards} (${playerCards.totalValueOfCards})
-      \n${dealerName}: -
-      \nPlayer wins!`
+      playerName + ': ' + playerCards.join(', ') + '(' + playerValueOfCards + ')\n' +
+      dealerName + ': -\n' +
+      playerName + ' wins!\n'
     )
   }
 
-  dealerWins (playerName, playerCards, dealerName, dealerCards) {
+  dealerWins (playerName, playerCards, playerValueOfCards, dealerName, dealerCards, dealerValueOfCards) {
     console.log(
-      `${playerName}: ${playerCards} (${playerCards.totalValueOfCards})
-      \n${dealerName}: ${dealerCards} (${dealerCards.totalValueOfCards})
-      \nDealer wins!`
+      playerName + ': ' + playerCards.join(', ') + ' (' + playerValueOfCards + ')\n' +
+      dealerName + ': ' + dealerCards.join(', ') + ' (' + dealerValueOfCards + ')\n' +
+      dealerName + ' wins!\n'
     )
   }
 }
