7849b57a27714a54e5cd6fb972ed0d6a0a96a549
mc222sn <mc222sn@student.lnu.se>
2018-10-22 16:46:22 +0200

    Changing code inside while loop

--- app.js
+++ app.js
@@ -15,27 +15,28 @@ let deck = new Deck()
 let dealer = Player('Dealer', 18)
 
 if (numberOfPlayers > 0) {
-    let start = true
+    let run = true
 } else {
-    let start = false
+    let run = false
 }
 
-while (start) {
+while (run) {
     deck.shuffle(deck.cards)
     if (deck.cards < 2) {
         deck.shuffle(deck.throwedCards) // kör shuffle på de kort som kastats i kasthögen
     } else {
-        // här körs spelet på som vanligt tills condition ovan är sant
         for (let a = 0; a < players.length; a++) {
             for (let b = 0; b < 4; b++) {
-                if (players[i].cards.length == 2) {
-                    // om korten är lika med två exakt så ska det evalueras
-                    // om spelaren ska ta mer kort
-                } // Kör en else if () här och kontrollera om spelarens värde på korten är mer än 21
                 players[i].insertCard(deck.getCard())
+                if (players[a].cards.length == 2) {
+                    // code
+                } else if (players[a].totalValueOfCards === 21) {
+                    console.log(`${players[a].name}: ${players[a].cards.toString()}\n${dealer.name}: -\nPlayer wins!`)
+                    break
+                }
             }   
         }
-        start = false // avslutar spelet då alla spelare spelat
+        run = false // avslutar spelet då alla spelare spelat
     }  
 }
 
