b6b90ae79029cb0ababd226ca56279c6d40c9de7
mc222sn <mc222sn@student.lnu.se>
2018-10-23 23:41:26 +0200

    Changing game structure

--- app.js
+++ app.js
@@ -1,54 +1,9 @@
 'use strict'
 
 const Game = require('./src/game')
-const Deck = require('./src/deck')
-const Player = require('./src/player')
 
-let game = new Game('21', 30)
-let deck = new Deck()
-let dealer = new Player('Dealer', 18)
+const amountOfPlayers = 30
 
-let players = []
-for (let i = 0; i < game.players; i++) {
-  //let randomNumber = Math.floor(Math.random() * 11) + 8
-  players[i] = new Player('Player #' + (i + 1), 20)
-}
+let game = new Game(amountOfPlayers)
 
-while (game.isRunning) {
-  deck.shuffle(deck.cards)
-
-  for (let a = 0; a < players.length; a++) {
-    let satisfied = undefined
-    for (let b = 0; b < 4; b++) {
-
-      if (deck.cards.length < 2) {
-        deck.combineCards()
-        deck.shuffle(deck.cards)
-        players[a].insertCard(deck.getCard(deck.cards))
-      } else {
-        players[a].insertCard(deck.getCard(deck.cards))
-      }
-
-
-      if (players[a].totalValueOfHand() > 21) {
-        console.log('Dealer wins!')
-      } else if (players[a].totalValueOfHand() < 21) {
-        console.log('Less than 21!')
-        
-        if (players[a].hand.length > 2) {
-          // if the totalValueOfHand is more or equal to stopValue
-          console.log('More than 2 cards in hand.')
-        } else if (players[a].hand.length === 2) {
-          // if the totalValueOfHand is more or equal to stopValue
-          console.log('Equal to two cards in hand.')
-        }
-
-      } else if (players[a].totalValueOfHand() === 21) {
-        console.log('Player wins!')
-      }
-
-    }
-    deck.throwUsedCards(players[a].hand)
-  }
-  game.isRunning = false
-}
+game.start()
--- src/game.js
+++ src/game.js
@@ -1,27 +1,82 @@
 'use strict'
 
+const Player = require('./player')
+const Deck = require('./deck')
+
 class Game {
-  constructor (name, players) {
-    this.name = name
-    this.players = players
+  constructor (numberOfPlayers) {
+    this.numberOfPlayers = numberOfPlayers
+    this.players = []
     this.isRunning = true
   }
 
-  playerWins (playerName, playerCards, playerValueOfCards, dealerName) {
+  createPlayers () {
+    for (let i = 0; i < this.numberOfPlayers; i++) {
+      //let randomNumber = Math.floor((Math.random() * 11) + 8)
+      this.players[i] = new Player('Player #' + (i + 1), 8)
+    }
+  }
+
+  start () {
+    while (this.isRunning) {
+      let deck = new Deck()
+      let dealer = new Player('Dealer', 18)
+      this.createPlayers()
+      
+      deck.shuffle(deck.cards)
+    
+      for (let a = 0; a < this.players.length; a++) {
+        let satisfied = undefined
+        for (let b = 0; b < 4; b++) {
+    
+          if (deck.cards.length < 2) {
+            deck.combineCards()
+            deck.shuffle(deck.cards)
+            this.players[a].insertCard(deck.getCard(deck.cards))
+          } else {
+            this.players[a].insertCard(deck.getCard(deck.cards))
+          }
+    
+    
+          if (this.players[a].totalValueOfHand() > 21) {
+            this.playerLost(this.players[a], dealer)
+          } else if (this.players[a].totalValueOfHand() < 21) {
+            
+            if (this.players[a].hand.length > 2) {
+              console.log('More than 2 cards in hand.\n')
+            } else if (this.players[a].hand.length === 2) {
+              console.log('Equal to two cards in hand.\n')
+            } else if (this.players[a].hand.length === 5 && players[a].totalValueOfHand() < 21) {
+              this.playerWon(this.players[a], dealer)
+            }
+    
+          } else if (this.players[a].totalValueOfHand() === 21) {
+            this.playerWon(this.players[a], dealer)
+          }
+    
+        }
+        deck.throwUsedCards(this.players[a].hand)
+      }
+      this.isRunning = false
+    }
+  }
+
+  playerWon(player, dealer) {
     console.log(
-      playerName + ': ' + playerCards.join(', ') + '(' + playerValueOfCards + ')\n' +
-      dealerName + ': -\n' +
-      playerName + ' wins!\n'
+      player.name + ': ' + player.hand.join(', ') + ' (' + player.totalValueOfHand() + ')\n' +
+      dealer.name + ': ' + dealer.hand.join(', ') + ' (' + dealer.totalValueOfHand() + ')\n' +
+      player.name + ' wins!\n'
     )
   }
 
-  dealerWins (playerName, playerCards, playerValueOfCards, dealerName, dealerCards, dealerValueOfCards) {
+  playerLost(player, dealer) {
     console.log(
-      playerName + ': ' + playerCards.join(', ') + ' (' + playerValueOfCards + ')\n' +
-      dealerName + ': ' + dealerCards.join(', ') + ' (' + dealerValueOfCards + ')\n' +
-      dealerName + ' wins!\n'
+      player.name + ': ' + player.hand.join(', ') + ' (' + player.totalValueOfHand() + ')\n' +
+      dealer.name + ': ' + dealer.hand.join(', ') + ' (' + dealer.totalValueOfHand() + ')\n' +
+      dealer.name + ' wins!\n'
     )
   }
+
 }
 
 module.exports = Game
